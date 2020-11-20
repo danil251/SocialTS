368515b01f5607bfb230d804d89bb14f2daae748
danil251 <slipitout@yandex.ru>
2020-11-20 17:26:05 +0500

    Updates

---
+++ precache-manifest.2733babd2003ad7d2f4f4fd0c47a74c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f701eed7e3b187b68fed08b1cdaa28e",
+    "url": "/danil251/SocialTS/index.html"
+  },
+  {
+    "revision": "8d7307d00679174b38b1",
+    "url": "/danil251/SocialTS/static/css/main.6cdef90f.chunk.css"
+  },
+  {
+    "revision": "0cfd035f23931f5a6b81",
+    "url": "/danil251/SocialTS/static/js/2.c4360ae8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/danil251/SocialTS/static/js/2.c4360ae8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d7307d00679174b38b1",
+    "url": "/danil251/SocialTS/static/js/main.f154a904.chunk.js"
+  },
+  {
+    "revision": "81f6f262ea5e91e402a7",
+    "url": "/danil251/SocialTS/static/js/runtime-main.c886b32f.js"
+  }
+]);
